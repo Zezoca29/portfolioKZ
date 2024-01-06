63af7b23063aa1fd5aafa5444ab657b50498baa9
Zezoka29 <kaiquezeza@gmail.com>
2024-01-05 22:19:02 -0300

    Portfólio 2.0

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 4000; // Change the port number
 
-// Servir arquivos estáticos no diretório src
+// Serve static files in the 'src' directory
 app.use(express.static(path.join(__dirname, 'src')));
 
-// Rota para o arquivo index.html
+// Route for the 'index.html' file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
-// Iniciar o servidor
+// Start the server
 app.listen(port, () => {
-    console.log(`Servidor iniciado em http://localhost:${port}`);
+    console.log(`Server started at http://localhost:${port}`);
 });
