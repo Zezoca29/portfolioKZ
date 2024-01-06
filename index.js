3bdb26b9cf7ebab7c5929c2b3e6c7400439a576a
Zezoka29 <kaiquezeza@gmail.com>
2024-01-05 22:48:25 -0300

    Atualização do index

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 4000; // Change the port number
+const port = process.env.PORT || 4000; // Porta dinâmica ou padrão 4000
 
-// Serve static files in the 'src' directory
+// Servir arquivos estáticos no diretório 'src'
 app.use(express.static(path.join(__dirname, 'src')));
 
-// Route for the 'index.html' file
+// Rota para o arquivo 'index.html'
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
-// Start the server
+// Iniciar o servidor
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+    console.log(`Servidor iniciado em http://localhost:${port}`);
 });
