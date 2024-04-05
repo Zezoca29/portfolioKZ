267888cbc5dc66c3cf8d4746b1d93936cf2fc609
Zezoca29 <kaiquezeza@gmail.com>
2024-04-05 15:48:09 -0300

    Sprint Engenheiro de software

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 4000; // Porta dinâmica ou padrão 4000
+
+// Servir arquivos estáticos no diretório 'src'
+app.use(express.static(path.join(__dirname, 'src')));
+
+// Rota para o arquivo 'index.html'
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+    console.log(`Servidor iniciado em http://localhost:${port}`);
+});
